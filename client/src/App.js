7997ab83e80a21afb7dd2d25e38c400f53d06d58
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-07-04 06:43:34 +0530

    modified

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a
+          href=""
+          className='logo'>
+          MYBLOG
+        </a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+
+      <div className='post'>
+      <div className="image"> 
+        <img
+          src='https://plus.unsplash.com/premium_photo-1665329006985-04f95dd59402?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D'
+          alt=''
+        />
+        </div>
+
+        <div className='text'>
+          <h2>full house battery backup coming last this year</h2>
+          <p className="info">
+            <a className="author">Dhanushkumar</a>
+            <time >20-07-2024 04:45</time>
+          </p>
+          <p className="summary">
+            When I was an elementary school, I have so many time to travel with
+            my family. But, Now something amazing happend. My family are broke
+            up. I felt face to reality that anyone else can hep us except
+            oursefl. So, I begin my life with my self. Some day I will go to
+            travel around the world. 
+          </p>
+        </div>
+      </div>
+      <div className='post'>
+      <div className="image"> 
+        <img
+          src='https://plus.unsplash.com/premium_photo-1665329006985-04f95dd59402?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D'
+          alt=''
+        />
+        </div>
+
+        <div className='text'>
+          <h2>full house battery backup coming last this year</h2>
+          <p className="info">
+            <a className="author">Dhanushkumar</a>
+            <time >20-07-2024 04:45</time>
+          </p>
+          <p className="summary">
+            When I was an elementary school, I have so many time to travel with
+            my family. But, Now something amazing happend. My family are broke
+            up. I felt face to reality that anyone else can hep us except
+            oursefl. So, I begin my life with my self. Some day I will go to
+            travel around the world. 
+          </p>
+        </div>
+      </div>
+      <div className='post'>
+      <div className="image"> 
+        <img
+          src='https://plus.unsplash.com/premium_photo-1665329006985-04f95dd59402?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D'
+          alt=''
+        />
+        </div>
+
+        <div className='text'>
+          <h2>full house battery backup coming last this year</h2>
+          <p className="info">
+            <a className="author">Dhanushkumar</a>
+            <time >20-07-2024 04:45</time>
+          </p>
+          <p className="summary">
+            When I was an elementary school, I have so many time to travel with
+            my family. But, Now something amazing happend. My family are broke
+            up. I felt face to reality that anyone else can hep us except
+            oursefl. So, I begin my life with my self. Some day I will go to
+         
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
